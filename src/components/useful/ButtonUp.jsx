70e74b1e15bd3e2d026017d353b639ea9ebb3caa
reimontools <ramon.fuentes.alarcon@gmail.com>
2021-02-21 14:34:24 +0000

    Button up
    Set useState in ButtonUp done

--- src/components/useful/ButtonUp.jsx
+++ src/components/useful/ButtonUp.jsx
@@ -1,61 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
-import { mediumScreenSize } from "../../config/param.js";
 import { BsChevronUp } from "react-icons/bs";
 
 const DivButtonUpStyled = styled.div `
-    width: 40px;
-    height: 40px;
-    background: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: rgb(99, 99, 99);
-    border-radius: 50%;
-    font-size: 20px;
     position: fixed;
     bottom: 30px;
     right: 30px;
-    cursor: pointer;
-    /*border: 1px solid transparent;*/
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: white;
+    color: rgb(99, 99, 99);
+    border-radius: 50%;
+    font-size: 20px;
+    transform: ${({ showMe }) => showMe ? 'scale(1)' : 'scale(0)'};
     transition: all 300ms ease;
-    transform: scale(0);
+    cursor: pointer;
     &:hover {
-        transform: scale(1.1);
-        /* border-color: rgba(0, 0, 0, 0.1); */
-    }
-    @media screen and (max-width: ${mediumScreenSize}) {
-        /* font-size: 30px;
-        letter-spacing: 3px; */
-    }
+        transform: scale(1.2);
+    };
 `;
 
 const ButtonUp = () => {
+    const [showMe, setShowMe] = useState(false);
 
     useEffect(() => {
-        const buttonUp = document.getElementById("button-up");
         window.onscroll = () => {
             const scroll = document.documentElement.scrollTop;
             if (scroll > 100) {
-                buttonUp.style.transform = "scale(1)";
+                setShowMe(true);
             } else if (scroll <= 100) {
-                buttonUp.style.transform = "scale(0)";
+                setShowMe(false);
             }
         };
     }, []);
 
-    const irArriba = () => {
-        const buttonUp = document.getElementById("button-up");
+    const up = () => {
+        setShowMe(false);
         var current = document.documentElement.scrollTop;
         if (current > 0) {
-            window.requestAnimationFrame(irArriba);
-            window.scrollTo(0, current - (current / 10));
-            buttonUp.style.transform = "scale(0)";
+            window.requestAnimationFrame(up);
+            window.scrollTo(0, current - (current / 8));
         };
     };
 
     return (
-        <DivButtonUpStyled id="button-up" onClick={() => irArriba()}>
+        <DivButtonUpStyled  showMe={ showMe } id="button-up" onClick={() => up()}>
             <BsChevronUp />
         </DivButtonUpStyled>
     );
